af27dea3bf2139c63149439b64e7d25a9f5cab14
carrot0822 <a384713827@qq.com>
2019-07-19 18:05:22 +0800

    列表检索和中图分类法完成部分 非空检测类未写

--- src/request/api/search.js
+++ src/request/api/search.js
@@ -4,23 +4,37 @@ var url = window.url
 
 const searchUrl = {
     collet:`${url}serviceforreadermodule/query/selectAllBookType`, // 中途分类法
-    allSearch:`${url}serviceforreadermodule/query/baseSelect`
+    allSearch:`${url}serviceforreadermodule/query/baseSelect`,
+    detail:`${url}serviceforreadermodule/query/getTableTwo`,
+    select:`${url}serviceforreadermodule/query/getTableOne`
 }
 export const searchInt = {
     collectInt: collect,
     allSearchInt:allSearch,
 }
-
-
+export function detailInt(data) {
+    return axios.get(searchUrl.detail,{
+        params:data
+    }).then((res) =>{
+        return Promise.resolve(res)
+    })
+}
+export function selectInt(data){
+    return axios.get(searchUrl.select,{
+        params:data
+    }).then((res)=>{
+        return Promise.resolve(res)
+    })
+}
 function collect (){
     return axios.get(searchUrl.collet).then((res) => {
         return Promise.resolve(res)
     })
 }
 
-function allSearch(obj) {
+function allSearch(data) {
     return axios.get(searchUrl.allSearch,{
-        params:obj
+        params:data
     }).then((res) => {
         return Promise.resolve(res)
     })
